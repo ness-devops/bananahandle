6a8950d0d39f132acfb65e8416a5360c3b2a9c3c
sachinlj <sachin.jadhav@ness.com>
2016-05-27 19:00:25 +0530

    apache conf

--- cookbooks/lamp-stack/attributes/default.rb
+++ cookbooks/lamp-stack/attributes/default.rb
@@ -1,1 +1,2 @@
-default["lamp-stack"]["sites"]["ness-devops.com"] = { "port" => 80, "servername" => "ness-devops.com", "serveradmin" => "webmaster@ness-devops.com" }
+default["lamp-stack"]["sites"]["ness-devops.com"] = { "port" => 80, "servername" => "ec2-52-87-151-232.compute-1.amazonaws.com", "serveradmin" => "webmaster@ness-devops.com" }
+default["lamp-stack"]["sites"]["ness-appsupport.com"] = { "port" => 80, "servername" => "ip-172-31-4-22.ec2.internal", "serveradmin" => "webmaster@ness-appsupport.com" }
--- cookbooks/lamp-stack/recipes/apache.rb
+++ cookbooks/lamp-stack/recipes/apache.rb
@@ -1,47 +1,51 @@
-
 package "apache2" do
  action :install
 end
 
 service "apache2" do
- action [:enable, start]
+ action [:enable, :start]
 end
 
-node["lamp-stack"]["sites"].each do |sitename, data|
-  document_root = "/var/www/html/#{sitename}"
+  document_root = "/var/www/html/ness-devops.com"
 
   directory document_root do
     mode "0755"
     recursive true
   end
 
-  template "/etc/apache2/sites-available/#{sitename}.conf" do
+  template "/etc/apache2/sites-available/ness-devops.com.conf" do
     source "virtualhosts.erb"
     mode "0644"
     variables(
       :document_root => document_root,
-      :port => data["port"],
-      :serveradmin => data["serveradmin"],
-      :servername => data["servername"]
+      :port => node["lamp-stack"]["sites"]["ness-devops.com"]["port"],
+      :serveradmin => node["lamp-stack"]["sites"]["ness-devops.com"]["serveradmin"],
+      :servername => node["lamp-stack"]["sites"]["ness-devops.com"]["servername"]
      )
     notifies :restart, "service[apache2]"
   end
  
   execute "enable-sites" do
-    command "a2ensite #{sitename}"
+    command "a2ensite ness-devops.com"
     action :nothing
   end
 
-  template "/etc/apache2/sites-available/#{sitename}.conf" do
+  template "/etc/apache2/sites-available/ness-devops.com.conf" do
     notifies :run, "execute[enable-sites]"
   end
  
-  directory "/var/www/html/#{sitename}/public_html" do
+  directory "/var/www/html/ness-devops.com/public_html" do
     action :create
   end
 
-  directory "/var/www/html/#{sitename}/logs" do
+  directory "/var/www/html/ness-devops.com/logs" do
     action :create
   end
-
-end
+  
+  # Write the home page.
+  file "/var/www/html/ness-devops.com/public_html/index.html" do
+    content '<html>This is a placeholder</html>'
+    mode '0644'
+    owner 'www-data'
+    group 'www-data'
+  end
